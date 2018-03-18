4c7c9777934d9c2501ca51bf9de6d96af0603afa
elzamin <elzamin006@mail.ru>
2018-03-18 19:51:57 +0300

    Update lab3.2.cpp

--- 20/lab3.2.cpp
+++ 20/lab3.2.cpp
@@ -5,7 +5,7 @@ int main() {
 	float A[65], B[65], min = -30.0, max = 47.5;
 	for (int i = 0; i < 65; i++)
 		A[i] = (float)rand() / (RAND_MAX + 1) * (max - min) + min;
-	for (i = 0; i < 65; i++)
+	for (int i = 0; i < 65; i++)
 	{
 		B[i] = 0;
 		if (A[i] > 25.8 && A[i] < 34.7)
